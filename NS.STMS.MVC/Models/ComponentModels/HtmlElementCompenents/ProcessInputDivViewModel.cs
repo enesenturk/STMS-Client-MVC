abfea1e780a98a0655bbd89a759e6a38d8aea923
NS <enesenturk61@gmail.com>
2024-05-29 18:09:51 +0300

    floating label dropdown css added

---
+++ NS.STMS.MVC/Models/ComponentModels/HtmlElementCompenents/ProcessInputDivViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace NS.STMS.MVC.Models.ComponentModels.HtmlElementCompenents
+{
+	public class ProcessInputDivViewModel
+	{
+
+		public string Id { get; set; }
+		public string Text { get; set; }
+		public string OnClick { get; set; }
+		public string BtnClass { get; set; }
+		public bool Disabled { get; set; }
+
+	}
+}
---
+++ NS.STMS.MVC/ViewComponents/HtmlElementCompenents/ProcessInputDivViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc;
+using NS.STMS.MVC.Extensions;
+using NS.STMS.MVC.Models.ComponentModels.HtmlElementCompenents;
+
+namespace NS.STMS.MVC.ViewComponents.HtmlElementCompenents
+{
+	public class ProcessInputDivViewComponent : ViewComponent
+	{
+
+		public ViewViewComponentResult Invoke(
+			string id,
+			string text,
+			string onClick,
+			string btnClass,
+			bool disabled = false
+			)
+		{
+			string path = GetType().GetViewComponentPath();
+
+			ProcessInputDivViewModel model = new ProcessInputDivViewModel
+			{
+				Id = id,
+				BtnClass = btnClass,
+				Text = text,
+				OnClick = onClick,
+				Disabled = disabled
+			};
+
+			return View(path, model);
+		}
+	}
+}
